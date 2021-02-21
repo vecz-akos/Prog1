026fab63706e8a99b34249fc2007721b73e19384
vecz-akos <oraveczakos2001@gmail.com>
2021-02-21 18:58:02 +0100

    apró javítások

--- week02-06/drill.cpp
+++ week02-06/drill.cpp
@@ -1,8 +1,8 @@
 // To compile, run this:
-// g++ drill.cpp Graph.cpp Window.cpp GUI.cpp Simple_window.cpp -o drill `fltk-config --ldflags --use-images`
+// g++ drill.cpp GUI/Graph.cpp GUI/Window.cpp GUI/GUI.cpp GUI/Simple_window.cpp -o drill `fltk-config --ldflags --use-images`
 
-#include "Simple_window.h"
-#include "Graph.h"
+#include "GUI/Simple_window.h"
+#include "GUI/Graph.h"
 
 #include <string>
 #include <iostream>
